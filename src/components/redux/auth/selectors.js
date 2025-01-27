f479ebab47d7ad2547a7c08a3a79c9663433e83f
Oksana <oksanavasilevna77@gmail.com>
2025-01-27 17:21:21 +0200

    add auth

---
+++ src/components/redux/auth/selectors.js
@@ -0,0 +1,5 @@
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+
+export const selectUser = (state) => state.auth.user;
+
+export const selectIsLoggedOut = (state) => state.auth.isLoggedOut;
