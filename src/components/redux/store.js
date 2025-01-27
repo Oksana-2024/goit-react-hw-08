0522f328de7a758d15a2de8ea6e965ce577608a6
Oksana <oksanavasilevna77@gmail.com>
2025-01-27 17:22:09 +0200

    add store

---
+++ src/components/redux/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import contactsReducer from "./contactsSlice";
+import filtersReducer from "./filtersSlice";
+
+
+
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+    auth: authReducer,
+  },
+  
+});
+
+
+export { store };
