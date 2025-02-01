8afdad65b4bb3117dccf05045019cb1a4d7b95cf
Oksana <oksanavasilevna77@gmail.com>
2025-02-01 17:34:11 +0200

    moving elements

--- src/components/redux/auth/selectors.js
+++
@@ -1,5 +0,0 @@
-export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-
-export const selectUser = (state) => state.auth.user;
-
-export const selectIsLoggedOut = (state) => state.auth.isLoggedOut;
--- src/components/redux/auth/slice.js
+++
@@ -1,9 +0,0 @@
-/* const auth ={
-    user: {
-      name: null,
-      email: null,
-    },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
-  } */
