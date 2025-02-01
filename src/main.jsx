b9fc5388039889dfb4199701ab8b5db6e0d53c2e
Oksana <oksanavasilevna77@gmail.com>
2025-02-01 17:35:59 +0200

    add settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "modern-normalize";
+import "./index.css";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.querySelector("#root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
