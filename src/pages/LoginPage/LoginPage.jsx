e4d5f11747a6f80dc9a4bfd95ee11b78b8408ae5
Oksana <oksanavasilevna77@gmail.com>
2025-02-03 15:17:20 +0200

    add padding

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,14 @@
 import Container from "../../components/Container/Container";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import s from "./LoginPage.module.css"
 
 const LoginPage = () => {
   return (
-    <Container>
-      <LoginForm />
-    </Container>
+   <div className={s.loginPage}>
+      <Container>
+        <LoginForm />
+      </Container>
+   </div>
   );
 };
 
