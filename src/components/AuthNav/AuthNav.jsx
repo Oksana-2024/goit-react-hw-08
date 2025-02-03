157e4d8149b921130aa45b36074ebacb34e1ccb0
Oksana <oksanavasilevna77@gmail.com>
2025-02-03 16:15:06 +0200

    update styles

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,20 +1,25 @@
 import { NavLink } from "react-router-dom";
 import s from "./AuthNav.module.css";
+import clsx from "clsx";
 
 const AuthNav = () => {
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
+
   return (
     <nav className={s.nav}>
       <ul className={s.navList}>
-       <li>
-          <NavLink className={s.link} to="/register">
+        <li>
+          <NavLink className={buildLinkClass} to="/register">
             Register
           </NavLink>
-       </li>
-       <li>
-          <NavLink className={s.link} to="/login">
+        </li>
+        <li>
+          <NavLink className={buildLinkClass} to="/login">
             Log In
           </NavLink>
-       </li>
+        </li>
       </ul>
     </nav>
   );
